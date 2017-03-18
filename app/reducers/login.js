9f586475754a235fb1a17b920dead9aed7e8bf32
lk <991920040@qq.com>
2017-03-18 21:13:13 +0800

    uodate

--- app/reducers/login.js
+++ app/reducers/login.js
@@ -2,8 +2,8 @@
 import * as actionTypes from '../const/actionTypes.js'
 
 const initialData = {
-	 username:null,  	
-	 loginstate:0  // 0未登录  1登录成功  -1 正在登录	
+	 username:null,
+	 loginstate:0  // 0未登录  1登录成功  -1 正在登录
 }
 
 export default function(state = initialData,action){
@@ -13,16 +13,14 @@ export default function(state = initialData,action){
 				loginstate:1,
 				username:action.username
 			}
-			break
 		case actionTypes.LOG_ING:
 			return{
 				loginstate:-1,
 				username:'正在登录'
-			}	
+			}
 		case actionTypes.LOG_OUT:
 			return initialData
-			break
 		default :
-			return initialData	
+			return initialData
 	}
 }
